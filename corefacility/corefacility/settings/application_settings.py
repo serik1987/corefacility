bff60534a37136a957a3a7f983c0c83dc1a86abc
serik1987@gmail.com <serik1987@gmail.com>
2021-12-08 08:17:00 +0300

    Fixed: failed to up the server

--- corefacility/corefacility/settings/application_settings.py
+++ corefacility/corefacility/settings/application_settings.py
@@ -18,7 +18,6 @@ MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
