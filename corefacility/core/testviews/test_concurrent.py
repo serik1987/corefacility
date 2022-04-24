13870ff3f409664c5d06912acd1972ffc7c5de38
serik1987@gmail.com <serik1987@gmail.com>
2022-04-24 07:01:30 +0300

    Fixed: concurrent test fails when DEBUG=no

--- corefacility/core/testviews/test_concurrent.py
+++ corefacility/core/testviews/test_concurrent.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+import logging
 from time import sleep
+
+from django.shortcuts import render
 
 from core.entity.log import Log
 
+logger = logging.getLogger("django.corefacility.test")
+
 
 def test_concurrent(request):
-    log = Log.current()
-    sleep(10.0)
-    print(id(log), log.id)
+    try:
+        log = Log.current()
+        sleep(10.0)
+        print(id(log), log.id)
+    except Exception as e:
+        logger.warning("The test is not possible during the GET request and when debug is OFF",
+                       extra={"request": request})
     return render(request, "core/tests/test.html")
