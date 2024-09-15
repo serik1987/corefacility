20d3353eab238d02730c8e053428de10f3fc7719
Sergei Kozhukhov <serik1987@gmail.com>
2024-09-15 04:19:59 +0300

    'files' property was added to the DataRecord class

--- src/ru/ihna/kozhukhov/core_application/entity/labjournal_record/data_record.py
+++ src/ru/ihna/kozhukhov/core_application/entity/labjournal_record/data_record.py
@@ -19,3 +19,13 @@ class DataRecord(Record):
     """
     If these fields are not filled by the server side's view layer the labjournal record can't be saved
     """
+
+    @property
+    def files(self):
+        """
+        Container that contains all files attached to this data record (an instance of the FileSet object)
+        """
+        from ru.ihna.kozhukhov.core_application.entity.labjournal_file import FileSet
+        file_set = FileSet()
+        file_set.record = self
+        return file_set
